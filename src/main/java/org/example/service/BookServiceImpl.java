cf0381f3954fb78048c56b8092c29af23a9f12c6
uvinduivan <uvinduivan456@gmail.com>
2024-02-26 21:08:50 +0530

    first commit

---
+++ src/main/java/org/example/service/BookServiceImpl.java
@@ -0,0 +1,11 @@
+package org.example.service;
+
+import org.example.dto.Book;
+
+public class BookServiceImpl implements BookService{
+    @Override
+    public void addBook(Book book){
+
+    }
+
+}
