3062b195c2457fb679319c2ec8fcde173777b6fd
uvinduivan <uvinduivan456@gmail.com>
2024-03-14 09:31:16 +0530

    system is ready to start......

--- src/main/java/org/example/service/BookServiceImpl.java
+++ src/main/java/org/example/service/BookServiceImpl.java
@@ -1,14 +1,35 @@
 package org.example.service;
 
 import org.example.dto.Book;
+import org.example.entity.BookEntity;
+import org.example.repository.BookRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 
 @Service
+
 public class BookServiceImpl implements BookService{
+
+
+
+    @Autowired
+    BookRepository repository;
+
+    ModelMapper mapper;
+
+    @Bean
+    public void setup(){
+        this.mapper=new ModelMapper();
+    }
+
+    
     @Override
     public void addBook(Book book){
-
+        BookEntity entity = mapper.map(book,BookEntity.class);
+        repository.save(entity);
     }
 
 }
